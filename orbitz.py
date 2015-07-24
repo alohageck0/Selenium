5a4951b8b1551a7d129a72a2a3ca37a686eee8a7
alohageck0 <royalfiish@gmail.com>
2015-07-23 20:25:44 -0700

    get price

--- orbitz.py
+++ orbitz.py
@@ -5,10 +5,14 @@ import time
 # Create object webdriver
 wd = webdriver.Firefox()
 wd.get('http://www.orbitz.com')
-time.sleep(4)
 wd.find_element_by_id('search.type.air').click()
+time.sleep(4)
 wd.find_element_by_name('ar.rt.leaveSlice.orig.key').send_keys('LAX')
 wd.find_element_by_name('ar.rt.leaveSlice.dest.key').send_keys('BOS')
 wd.find_element_by_name('ar.rt.leaveSlice.date').send_keys('07/31/15')
 wd.find_element_by_name('ar.rt.returnSlice.date').send_keys('08/31/15')
 wd.find_element_by_name('search').click()
+time.sleep(8)
+price = wd.find_element_by_xpath("//*[@id='preMatrix']/div[4]/div[2]/span").text
+print(price)
+wd.quit()
