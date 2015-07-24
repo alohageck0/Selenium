a5aa8969209d2e92e11c7683171a41b5fdab7d16
alohageck0 <royalfiish@gmail.com>
2015-07-23 20:42:19 -0700

    search

--- beepi.py
+++ beepi.py
@@ -1,8 +1,15 @@
 __author__ = 'royalfiish'
 from selenium import webdriver
+import time
 
 wd = webdriver.Firefox()
 wd.get("http://www.beepi.com")
+time.sleep(5)
+
+# click BUY
+wd.find_element_by_xpath(".//*[@id='headerlinks']/li[12]/a").click()
 
 
+wd.find_element_by_id('searchInput').send_keys("volkswagen")
+time.sleep(2)
 wd.quit()
